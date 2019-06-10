bf3bdf523dec04ba77767e82d1a97b11c61534bd
Mr.Chung <2286732552@qq.com>
2019-06-10 13:18:53 +0800

    模块化

--- api/menu.php
+++ api/menu.php
@@ -1,185 +1,192 @@
 <?php
 
-$menuList = [
-    'test' => [
-        'title' => '测试管理',
+$listList = [
+    'homeInfo'   => [
+        'title' => '首页',
         'icon'  => '&#xe634;',
-        'menu'  => [
-            [
-                'title'  => '按钮列表',
-                'href'   => 'page/button.html',
-                'icon'   => '&#xe634;',
-                'target' => '_self',
-            ],
-            [
-                'title'    => 'UI管理',
-                'href'     => '',
-                'icon'     => '&#xe634;',
-                'target'   => '',
-                'children' => [
-                    [
-                        'title'  => '按钮',
-                        'href'   => 'page/button.html',
-                        'icon'   => '&#xe634;',
-                        'target' => '_self',
-                    ],
-                    [
-                        'title'  => '表单',
-                        'href'   => 'page/form.html',
-                        'icon'   => '&#xe634;',
-                        'target' => '_self',
-                    ],
-                    [
-                        'title'  => '弹出层',
-                        'href'   => 'page/layer.html',
-                        'icon'   => '&#xe634;',
-                        'target' => '_self',
-                    ],
-                    [
-                        'title'  => '静态表格',
-                        'href'   => 'page/table.html',
-                        'icon'   => '&#xe634;',
-                        'target' => '_self',
-                    ],
+        'href'  => 'page/welcome.html',
+    ],
+    'moduleInfo' => [
+        'ceshi'   => [
+            'title' => '测试管理',
+            'icon'  => '&#xe634;',
+            'list'  => [
+                [
+                    'title'  => '按钮列表',
+                    'href'   => 'page/button.html',
+                    'icon'   => '&#xe634;',
+                    'target' => '_self',
                 ],
-            ],
-            [
-                'title'    => '测试无限层',
-                'href'     => '',
-                'icon'     => '&#xe634;',
-                'target'   => '',
-                'children' => [
-                    [
-                        'title'  => '按钮1',
-                        'href'   => 'page/button.html',
-                        'icon'   => '&#xe634;',
-                        'target' => '_self',
-                        'children' => [
-                            [
-                                'title'  => '按钮2',
-                                'href'   => 'page/button.html',
-                                'icon'   => '&#xe634;',
-                                'target' => '_self',
-                                'children' => [
-                                    [
-                                        'title'  => '按钮3',
-                                        'href'   => 'page/button.html',
-                                        'icon'   => '&#xe634;',
-                                        'target' => '_self',
-                                    ],
-                                    [
-                                        'title'  => '表单4',
-                                        'href'   => 'page/form.html',
-                                        'icon'   => '&#xe634;',
-                                        'target' => '_self',
-                                    ],
-                                ],
-                            ],
-                            [
-                                'title'  => '表单5',
-                                'href'   => 'page/form.html',
-                                'icon'   => '&#xe634;',
-                                'target' => '_self',
-                            ],
+                [
+                    'title'    => 'UI管理',
+                    'href'     => '',
+                    'icon'     => '&#xe634;',
+                    'target'   => '',
+                    'children' => [
+                        [
+                            'title'  => '按钮',
+                            'href'   => 'page/button.html',
+                            'icon'   => '&#xe634;',
+                            'target' => '_self',
+                        ],
+                        [
+                            'title'  => '表单',
+                            'href'   => 'page/form.html',
+                            'icon'   => '&#xe634;',
+                            'target' => '_self',
+                        ],
+                        [
+                            'title'  => '弹出层',
+                            'href'   => 'page/layer.html',
+                            'icon'   => '&#xe634;',
+                            'target' => '_self',
+                        ],
+                        [
+                            'title'  => '静态表格',
+                            'href'   => 'page/table.html',
+                            'icon'   => '&#xe634;',
+                            'target' => '_self',
                         ],
                     ],
-                    [
-                        'title'  => '表单6',
-                        'href'   => 'page/form.html',
-                        'icon'   => '&#xe634;',
-                        'target' => '_self',
+                ],
+                [
+                    'title'    => '测试无限层',
+                    'href'     => '',
+                    'icon'     => '&#xe634;',
+                    'target'   => '',
+                    'children' => [
+                        [
+                            'title'    => '按钮1',
+                            'href'     => 'page/button.html',
+                            'icon'     => '&#xe634;',
+                            'target'   => '_self',
+                            'children' => [
+                                [
+                                    'title'    => '按钮2',
+                                    'href'     => 'page/button.html',
+                                    'icon'     => '&#xe634;',
+                                    'target'   => '_self',
+                                    'children' => [
+                                        [
+                                            'title'  => '按钮3',
+                                            'href'   => 'page/button.html',
+                                            'icon'   => '&#xe634;',
+                                            'target' => '_self',
+                                        ],
+                                        [
+                                            'title'  => '表单4',
+                                            'href'   => 'page/form.html',
+                                            'icon'   => '&#xe634;',
+                                            'target' => '_self',
+                                        ],
+                                    ],
+                                ],
+                                [
+                                    'title'  => '表单5',
+                                    'href'   => 'page/form.html',
+                                    'icon'   => '&#xe634;',
+                                    'target' => '_self',
+                                ],
+                            ],
+                        ],
+                        [
+                            'title'  => '表单6',
+                            'href'   => 'page/form.html',
+                            'icon'   => '&#xe634;',
+                            'target' => '_self',
+                        ],
                     ],
                 ],
             ],
         ],
-    ],
-    'setting' => [
-        'title' => '设置管理',
-        'icon'  => '&#xe634;',
-        'menu'  => [
-            [
-                'title'  => '按钮列表【setting】',
-                'href'   => 'page/button.html',
-                'icon'   => '&#xe634;',
-                'target' => '_self',
-            ],
-            [
-                'title'    => 'UI管理【setting】',
-                'href'     => '',
-                'icon'     => '&#xe634;',
-                'target'   => '',
-                'children' => [
-                    [
-                        'title'  => '按钮',
-                        'href'   => 'page/button.html',
-                        'icon'   => '&#xe634;',
-                        'target' => '_self',
-                    ],
-                    [
-                        'title'  => '表单',
-                        'href'   => 'page/form.html',
-                        'icon'   => '&#xe634;',
-                        'target' => '_self',
-                    ],
-                    [
-                        'title'  => '弹出层',
-                        'href'   => 'page/layer.html',
-                        'icon'   => '&#xe634;',
-                        'target' => '_self',
-                    ],
-                    [
-                        'title'  => '静态表格',
-                        'href'   => 'page/table.html',
-                        'icon'   => '&#xe634;',
-                        'target' => '_self',
-                    ],
+        'setting' => [
+            'title' => '设置管理',
+            'icon'  => '&#xe634;',
+            'list'  => [
+                [
+                    'title'  => '按钮列表【setting】',
+                    'href'   => 'page/button.html',
+                    'icon'   => '&#xe634;',
+                    'target' => '_self',
                 ],
-            ],
-            [
-                'title'    => '测试无限层【setting】',
-                'href'     => '',
-                'icon'     => '&#xe634;',
-                'target'   => '',
-                'children' => [
-                    [
-                        'title'  => '按钮1',
-                        'href'   => 'page/button.html',
-                        'icon'   => '&#xe634;',
-                        'target' => '_self',
-                        'children' => [
-                            [
-                                'title'  => '按钮2',
-                                'href'   => 'page/button.html',
-                                'icon'   => '&#xe634;',
-                                'target' => '_self',
-                                'children' => [
-                                    [
-                                        'title'  => '按钮3',
-                                        'href'   => 'page/button.html',
-                                        'icon'   => '&#xe634;',
-                                        'target' => '_self',
-                                    ],
-                                    [
-                                        'title'  => '表单4',
-                                        'href'   => 'page/form.html',
-                                        'icon'   => '&#xe634;',
-                                        'target' => '_self',
-                                    ],
-                                ],
-                            ],
-                            [
-                                'title'  => '表单5',
-                                'href'   => 'page/form.html',
-                                'icon'   => '&#xe634;',
-                                'target' => '_self',
-                            ],
+                [
+                    'title'    => 'UI管理【setting】',
+                    'href'     => '',
+                    'icon'     => '&#xe634;',
+                    'target'   => '',
+                    'children' => [
+                        [
+                            'title'  => '按钮',
+                            'href'   => 'page/button.html',
+                            'icon'   => '&#xe634;',
+                            'target' => '_self',
+                        ],
+                        [
+                            'title'  => '表单',
+                            'href'   => 'page/form.html',
+                            'icon'   => '&#xe634;',
+                            'target' => '_self',
+                        ],
+                        [
+                            'title'  => '弹出层',
+                            'href'   => 'page/layer.html',
+                            'icon'   => '&#xe634;',
+                            'target' => '_self',
+                        ],
+                        [
+                            'title'  => '静态表格',
+                            'href'   => 'page/table.html',
+                            'icon'   => '&#xe634;',
+                            'target' => '_self',
                         ],
                     ],
-                    [
-                        'title'  => '表单6',
-                        'href'   => 'page/form.html',
-                        'icon'   => '&#xe634;',
-                        'target' => '_self',
+                ],
+                [
+                    'title'    => '测试无限层【setting】',
+                    'href'     => '',
+                    'icon'     => '&#xe634;',
+                    'target'   => '',
+                    'children' => [
+                        [
+                            'title'    => '按钮1',
+                            'href'     => 'page/button.html',
+                            'icon'     => '&#xe634;',
+                            'target'   => '_self',
+                            'children' => [
+                                [
+                                    'title'    => '按钮2',
+                                    'href'     => 'page/button.html',
+                                    'icon'     => '&#xe634;',
+                                    'target'   => '_self',
+                                    'children' => [
+                                        [
+                                            'title'  => '按钮3',
+                                            'href'   => 'page/button.html',
+                                            'icon'   => '&#xe634;',
+                                            'target' => '_self',
+                                        ],
+                                        [
+                                            'title'  => '表单4',
+                                            'href'   => 'page/form.html',
+                                            'icon'   => '&#xe634;',
+                                            'target' => '_self',
+                                        ],
+                                    ],
+                                ],
+                                [
+                                    'title'  => '表单5',
+                                    'href'   => 'page/form.html',
+                                    'icon'   => '&#xe634;',
+                                    'target' => '_self',
+                                ],
+                            ],
+                        ],
+                        [
+                            'title'  => '表单6',
+                            'href'   => 'page/form.html',
+                            'icon'   => '&#xe634;',
+                            'target' => '_self',
+                        ],
                     ],
                 ],
             ],
@@ -187,4 +194,4 @@ $menuList = [
     ],
 ];
 
-die(json_encode($menuList, JSON_UNESCAPED_UNICODE));
+die(json_encode($listList, JSON_UNESCAPED_UNICODE));
