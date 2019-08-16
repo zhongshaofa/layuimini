115559338758f460ca7bf1dd625f5921538d4e1a
Mr.Chung <2286732552@qq.com>
2019-08-16 10:30:46 +0800

    删除无效代码

--- js/layuimini.js
+++ js/layuimini.js
@@ -360,25 +360,6 @@ layui.define(["element", "jquery", "layer"], function (exports) {
         };
 
         /**
-         * 判断链接是否有效
-         * @param url
-         * @returns {boolean}
-         */
-        this.checkUrl = function (url) {
-            var msg = true;
-            $.ajax({
-                url: url,
-                type: 'get',
-                contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-                async: false,
-                error: function (xhr, textstatus, thrown) {
-                    msg = 'Status:' + xhr.status + '，' + xhr.statusText + '，请稍后再试！';
-                }
-            });
-            return msg;
-        };
-
-        /**
          * 成功
          * @param title
          * @returns {*}
@@ -395,103 +376,6 @@ layui.define(["element", "jquery", "layer"], function (exports) {
         this.msg_error = function (title) {
             return layer.msg(title, {icon: 2, shade: this.shade, scrollbar: false, time: 3000, shadeClose: true});
         };
-
-        /**
-         * 选项卡滚动
-         */
-        this.tabRoll = function () {
-            $(window).on("resize", function (event) {
-                var topTabsBox = $("#top_tabs_box"),
-                    topTabsBoxWidth = $("#top_tabs_box").width(),
-                    topTabs = $("#top_tabs"),
-                    topTabsWidth = $("#top_tabs").width(),
-                    tabLi = topTabs.find("li.layui-this"),
-                    top_tabs = document.getElementById("top_tabs"),
-                    event = event || window.event;
-
-                if (topTabsWidth > topTabsBoxWidth) {
-                    if (tabLi.position().left > topTabsBoxWidth || tabLi.position().left + topTabsBoxWidth > topTabsWidth) {
-                        topTabs.css("left", topTabsBoxWidth - topTabsWidth);
-                    } else {
-                        topTabs.css("left", -tabLi.position().left);
-                    }
-                    //拖动效果
-                    var flag = false;
-                    var cur = {
-                        x: 0,
-                        y: 0
-                    }
-                    var nx, dx, x;
-
-                    function down(event) {
-                        flag = true;
-                        var touch;
-                        if (event.touches) {
-                            touch = event.touches[0];
-                        } else {
-                            touch = event;
-                        }
-                        cur.x = touch.clientX;
-                        dx = top_tabs.offsetLeft;
-                    }
-
-                    function move(event) {
-                        var self = this;
-                        if (flag) {
-                            window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
-                            var touch;
-                            if (event.touches) {
-                                touch = event.touches[0];
-                            } else {
-                                touch = event;
-                            }
-                            nx = touch.clientX - cur.x;
-                            x = dx + nx;
-                            if (x > 0) {
-                                x = 0;
-                            } else {
-                                if (x < topTabsBoxWidth - topTabsWidth) {
-                                    x = topTabsBoxWidth - topTabsWidth;
-                                } else {
-                                    x = dx + nx;
-                                }
-                            }
-                            top_tabs.style.left = x + "px";
-                            //阻止页面的滑动默认事件
-                            document.addEventListener("touchmove", function () {
-                                event.preventDefault();
-                            }, false);
-                        }
-                    }
-
-                    //鼠标释放时候的函数
-                    function end() {
-                        flag = false;
-                    }
-
-                    //pc端拖动效果
-                    topTabs.on("mousedown", down);
-                    topTabs.on("mousemove", move);
-                    $(document).on("mouseup", end);
-                    //移动端拖动效果
-                    topTabs.on("touchstart", down);
-                    topTabs.on("touchmove", move);
-                    topTabs.on("touchend", end);
-                } else {
-                    //移除pc端拖动效果
-                    topTabs.off("mousedown", down);
-                    topTabs.off("mousemove", move);
-                    topTabs.off("mouseup", end);
-                    //移除移动端拖动效果
-                    topTabs.off("touchstart", down);
-                    topTabs.off("touchmove", move);
-                    topTabs.off("touchend", end);
-                    topTabs.removeAttr("style");
-                    return false;
-                }
-            }).resize();
-        };
-
 
     };
 
