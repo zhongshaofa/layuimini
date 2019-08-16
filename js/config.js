4cd7ee60d538ca129f56fe30dfd36e45d65189ab
Mr.Chung <2286732552@qq.com>
2019-08-16 14:56:42 +0800

    修复自定义layui模块冲突问题

---
+++ js/config.js
@@ -0,0 +1,10 @@
+layui.config({
+    base: "/lib/lay-module/",
+    version: true
+}).extend({
+    layuimini: "layuimini/layuimini", // layuimini扩展
+    step: 'step-lay/step', // 分步表单扩展
+    treetable: 'treetable-lay/treetable', //table树形扩展
+    tableSelect: 'tableSelect/tableSelect', // table选择扩展
+    iconPickerFa: 'iconPicker/iconPickerFa', // fa图标选择扩展
+});
