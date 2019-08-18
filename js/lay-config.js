dd5220502ab3b0a9b252fed876fcbce5fc2d76c1
Mr.Chung <2286732552@qq.com>
2019-08-18 12:55:59 +0800

    添加漏掉文件

---
+++ js/lay-config.js
@@ -0,0 +1,27 @@
+/**
+ * date:2019/08/16
+ * author:Mr.Chung
+ * description:此处放layui自定义扩展
+ */
+
+window.rootPath = (function () {
+    var curWwwPath = window.document.location.href,
+        pathName = window.document.location.pathname;
+    var localhostPaht = curWwwPath.substring(0, curWwwPath.indexOf(pathName)),
+        projectName = pathName.substring(0, pathName.substr(1).indexOf('/') + 1);
+    return localhostPaht + projectName;
+})();
+
+layui.config({
+    base: rootPath + "/lib/lay-module/",
+    version: true
+}).extend({
+    layuimini: "layuimini/layuimini", // layuimini扩展
+    step: 'step-lay/step', // 分步表单扩展
+    treetable: 'treetable-lay/treetable', //table树形扩展
+    tableSelect: 'tableSelect/tableSelect', // table选择扩展
+    iconPickerFa: 'iconPicker/iconPickerFa', // fa图标选择扩展
+    echarts: 'echarts/echarts', // echarts图表扩展
+    echartsTheme: 'echarts/echartsTheme', // echarts图表主题扩展
+    wangEditor: 'wangEditor/wangEditor', // wangEditor富文本扩展
+});
