9f67e078b76241325b8416443bd6d487164877f8
Mr.Chung <chung@99php.cn>
2020-01-03 16:28:05 +0800

    新增全屏以及新主頁

--- js/lay-module/layuimini/layuimini.js
+++ js/lay-module/layuimini/layuimini.js
@@ -437,7 +437,7 @@ layui.define(["element", "jquery", "layer"], function (exports) {
              * @returns {boolean}
              */
             this.href = function (href, title) {
-                layuimini.initPageTitle(href,title);
+                layuimini.initPageTitle(href, title);
                 layuimini.initConten(href);
                 layuimini.initDevice();
             };
@@ -511,6 +511,50 @@ layui.define(["element", "jquery", "layer"], function (exports) {
              */
             this.msg_error = function (title) {
                 return layer.msg(title, {icon: 2, shade: this.shade, scrollbar: false, time: 3000, shadeClose: true});
+            };
+
+            /**
+             * 进入全屏
+             */
+            this.fullScreen = function () {
+                var el = document.documentElement;
+                var rfs = el.requestFullScreen || el.webkitRequestFullScreen;
+                if (typeof rfs != "undefined" && rfs) {
+                    rfs.call(el);
+                } else if (typeof window.ActiveXObject != "undefined") {
+                    var wscript = new ActiveXObject("WScript.Shell");
+                    if (wscript != null) {
+                        wscript.SendKeys("{F11}");
+                    }
+                } else if (el.msRequestFullscreen) {
+                    el.msRequestFullscreen();
+                } else if (el.oRequestFullscreen) {
+                    el.oRequestFullscreen();
+                } else {
+                    layuimini.msg_error('浏览器不支持全屏调用！');
+                }
+            };
+
+            /**
+             * 退出全屏
+             */
+            this.exitFullScreen = function () {
+                var el = document;
+                var cfs = el.cancelFullScreen || el.webkitCancelFullScreen || el.exitFullScreen;
+                if (typeof cfs != "undefined" && cfs) {
+                    cfs.call(el);
+                } else if (typeof window.ActiveXObject != "undefined") {
+                    var wscript = new ActiveXObject("WScript.Shell");
+                    if (wscript != null) {
+                        wscript.SendKeys("{F11}");
+                    }
+                } else if (el.msExitFullscreen) {
+                    el.msExitFullscreen();
+                } else if (el.oRequestFullscreen) {
+                    el.oCancelFullScreen();
+                } else {
+                    layuimini.msg_error('浏览器不支持全屏调用！');
+                }
             };
 
         };
@@ -685,6 +729,22 @@ layui.define(["element", "jquery", "layer"], function (exports) {
             $(this).attr('class', 'layui-this');
             sessionStorage.setItem('layuiminiBgcolorId', bgcolorId);
             layuimini.initBgColor();
+        });
+
+        /**
+         * 全屏
+         */
+        $('body').on('click', '[data-check-screen]', function () {
+            var check = $(this).attr('data-check-screen');
+            if (check == 'full') {
+                layuimini.fullScreen();
+                $(this).attr('data-check-screen', 'exit');
+                $(this).html('<i class="fa fa-compress"></i>');
+            } else {
+                layuimini.exitFullScreen();
+                $(this).attr('data-check-screen', 'full');
+                $(this).html('<i class="fa fa-arrows-alt"></i>');
+            }
         });
 
         exports("layuimini", layuimini);
