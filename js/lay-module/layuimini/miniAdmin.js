a66b18f27ca32abcac5234fefb337b1b9e4bec29
chung <chung@99php.cn>
2020-04-09 10:34:17 +0800

    修改

--- js/lay-module/layuimini/miniAdmin.js
+++ js/lay-module/layuimini/miniAdmin.js
@@ -284,7 +284,6 @@ layui.define(["jquery", "miniMenu", "element","miniTab", "miniTheme"], function 
                 var classInfo = $(this).attr('class'),
                     tips = $(this).prop("innerHTML"),
                     isShow = $('.layuimini-tool i').attr('data-side-fold');
-                console.log(tips)
                 if (isShow == 0 && tips) {
                     tips = "<ul class='layuimini-menu-left-zoom layui-nav layui-nav-tree layui-this'><li class='layui-nav-item layui-nav-itemed'>"+tips+"</li></ul>" ;
                     window.openTips = layer.tips(tips, $(this), {
